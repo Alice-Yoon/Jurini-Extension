133076229cfb323d8e238962495446b9cd6ee7e5
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2020-08-17 14:33:40 +0900

    develop branch 생성 & App 컴포넌트 layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 
+import NavBar from './components/pages/NavBar/NavBar';
+import Home from './components/pages/Home/Home';
+import Search from './components/pages/Search/Search';
+import Introduction from './components/pages/Introduction/Introduction';
+import CompanyDetails from './components/pages/CompanyDetails/CompanyDetails';
 
 
 function App(props) {
   return (
     <div className={props.className}>
       <Router>
-      Jurini Extension
-
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/introduction" component={Introduction} />
+          <Route exact path="/details" component={CompanyDetails} />
+        </Switch>
       </Router>
     </div>
   );
